76c89a19541eb7dae02c3d1580527f9ccae1d108
Zhihao Yuan <zhihao.yuan@broadcom.com>
2021-12-14 13:10:34 -0800

    feat: pull in the latest source

--- include/function_ref.h
+++ include/function_ref.h
@@ -1,8 +1,127 @@
-﻿// function_ref_nontype.h : Include file for standard system include files,
-// or project specific include files.
+﻿#pragma once
 
-#pragma once
+#include <functional>
+#include <utility>
 
-#include <iostream>
+template <auto V> struct in_place_value_t
+{
+    explicit in_place_value_t() = default;
+};
 
-// TODO: Reference additional headers your program requires here.
+template <auto V> inline constexpr in_place_value_t<V> in_place_value{};
+
+template <class R, class F, class... Args>
+requires std::is_invocable_r_v<R, F, Args...>
+constexpr R std_invoke_r(F &&f, Args &&...args) noexcept(
+    std::is_nothrow_invocable_r_v<R, F, Args...>)
+{
+    if constexpr (std::is_void_v<R>)
+        std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+    else
+        return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+template <class Sig> class function_ref;
+
+template <class R, class... Args> class function_ref<R(Args...)>
+{
+    union storage {
+        void *p_ = nullptr;
+        void const *cp_;
+        void (*fp_)();
+
+        constexpr storage() noexcept = default;
+
+        template <class T>
+        requires std::is_object_v<T>
+        constexpr explicit storage(T *p) noexcept : p_(p)
+        {
+        }
+
+        template <class T>
+        requires std::is_object_v<T>
+        constexpr explicit storage(T const *p) noexcept : cp_(p)
+        {
+        }
+
+        template <class T>
+        requires std::is_function_v<T>
+        constexpr explicit storage(T *p) noexcept
+            : fp_(reinterpret_cast<decltype(fp_)>(p))
+        {
+        }
+
+    } obj_;
+
+    template <class T> constexpr static auto get(storage obj)
+    {
+        if constexpr (std::is_const_v<T>)
+            return static_cast<T *>(obj.cp_);
+        else if constexpr (std::is_object_v<T>)
+            return static_cast<T *>(obj.p_);
+        else
+            return reinterpret_cast<T *>(obj.fp_);
+    }
+
+    typedef R fn_t(storage, Args...);
+    fn_t *fptr_ = nullptr;
+
+  public:
+    function_ref() = default;
+
+    template <class F, class T = std::remove_reference_t<F>>
+    function_ref(F &&f) noexcept
+        requires(!std::is_same_v<std::remove_cv_t<T>, function_ref> and
+                 std::is_invocable_r_v<R, F, Args...> and
+                 !(std::is_pointer_v<T> and
+                       std::is_function_v<std::remove_pointer_t<T>> or
+                   std::is_member_pointer_v<T>))
+        : obj_(std::addressof(f)), fptr_([](storage fn_, Args... args) {
+              return std_invoke_r<R>(*get<T>(fn_), std::forward<Args>(args)...);
+          })
+    {
+    }
+
+    template <auto F>
+    constexpr function_ref(in_place_value_t<F>) noexcept requires
+        std::is_invocable_r_v<R, decltype(F), Args...>
+        : fptr_([](storage, Args... args) {
+            return std_invoke_r<R>(F, std::forward<Args>(args)...);
+        })
+    {
+    }
+
+    template <auto F, class T>
+    function_ref(in_place_value_t<F>, T &obj) noexcept requires
+        std::is_invocable_r_v<R, decltype(F), decltype(obj), Args...>
+        : obj_(std::addressof(obj)), fptr_([](storage this_, Args... args) {
+            return std_invoke_r<R>(F, *(get<T>(this_)),
+                                   std::forward<Args>(args)...);
+        })
+    {
+    }
+
+    template <auto F, class T>
+    function_ref(in_place_value_t<F>, T *obj) noexcept requires
+        std::is_invocable_r_v<R, decltype(F), decltype(obj), Args...> and
+        std::is_member_pointer_v<decltype(F)>
+        : obj_(obj), fptr_([](storage this_, Args... args) {
+            return std_invoke_r<R>(F, get<T>(this_),
+                                   std::forward<Args>(args)...);
+        })
+    {
+    }
+
+    template <auto F, class T>
+    function_ref(in_place_value_t<F> f,
+                 std::reference_wrapper<T> obj) noexcept requires
+        std::is_invocable_r_v<R, decltype(F), decltype(obj), Args...> and
+        std::is_member_pointer_v<decltype(F)> : function_ref(f, obj.get())
+    {
+    }
+
+    constexpr R operator()(Args... args) const
+    {
+        return fptr_(obj_, std::forward<Args>(args)...);
+    }
+};
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,134 @@
-﻿// function_ref_nontype.cpp : Defines the entry point for the application.
-//
+﻿#include "function_ref.h"
 
-#include "function_ref.h"
+#include <cstdio>
 
-using namespace std;
+#ifdef _MSC_VER
+#define BODY() std::puts(__FUNCSIG__)
+#else
+#define BODY() std::puts(__PRETTY_FUNCTION__)
+#endif
+
+void foo(function_ref<int()> f)
+{
+    f();
+}
+
+int f()
+{
+    return BODY();
+}
+
+template <class T> T g()
+{
+    return BODY();
+}
+
+struct A
+{
+    int g()
+    {
+        return BODY();
+    }
+
+    int k() const
+    {
+        return BODY();
+    }
+
+    int data = 99;
+};
+
+int h(A)
+{
+    return BODY();
+}
+
+struct C
+{
+    int operator()()
+    {
+        return BODY();
+    }
+
+    int operator()() const
+    {
+        return BODY();
+    }
+};
+
+struct Wat
+{
+    operator auto() const
+    {
+        return f;
+    }
+};
+
+constexpr int cf()
+{
+    return 2;
+}
+
+struct B
+{
+    int data = 101;
+};
+
+constexpr int constexpr_test1()
+{
+    function_ref<int()> fr;
+    fr = in_place_value<f>;  // f is not constexpr
+    fr = in_place_value<cf>; // cf is
+    return fr();
+}
+
+constexpr int constexpr_test2()
+{
+    B b;
+    function_ref<int(B)> fmr = in_place_value<&B::data>;
+    return fmr(b);
+}
+
+static_assert(constexpr_test1() == 2);
+static_assert(constexpr_test2() == 101);
+
+struct X
+{
+    void f()
+    {
+        BODY();
+    }
+    void g()
+    {
+        BODY();
+    }
+};
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    A a;
+    A const b;
+    foo(in_place_value<f>);
+    foo(in_place_value<g<int>>);
+    foo({in_place_value<&A::g>, a});
+    foo({in_place_value<&A::g>, &a});
+    foo({in_place_value<&A::k>, b});
+    foo({in_place_value<&A::k>, &b});
+    foo({in_place_value<&A::data>, a});
+    foo({in_place_value<&A::data>, std::ref(a)});
+    foo({in_place_value<&A::data>, std::cref(a)});
+    foo({in_place_value<h>, a});
+
+    foo(f);
+    foo([] { return BODY(); });
+    C c;
+    C const cc;
+    foo(c);
+    foo(cc);
+
+    X my_x_obj;
+    function_ref<void()> fr{in_place_value<&X::f>, &my_x_obj};
+    fr();
+    fr = {in_place_value<&X::g>, &my_x_obj};
+    fr();
 }
