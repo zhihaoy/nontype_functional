2928b8f7c19f5baedfaec7035747df9de557bbfe
Zhihao Yuan <zhihao.yuan@broadcom.com>
2022-01-28 20:26:28 -0800

    test: migrate noexcept tests to ut

--- tests/function_ref/main.cpp
+++ tests/function_ref/main.cpp
@@ -1,7 +1,6 @@
 ﻿#include "common_callables.h"
 
 void test_nontype();
-void test_noexcept();
 void test_safety();
 void test_const_noexcept();
 void test_call_pattern();
@@ -9,7 +8,6 @@ void test_call_pattern();
 int main()
 {
     test_nontype();
-    test_noexcept();
     test_safety();
     test_const_noexcept();
     test_call_pattern();
--- tests/function_ref/test_noexcept.cpp
+++ tests/function_ref/test_noexcept.cpp
@@ -1,57 +1,95 @@
 #include "common_callables.h"
 
+constexpr auto call = [](function_ref<int() noexcept> f)
+{
+    static_assert(noexcept(f()));
+    return f();
+};
+
 int f_good() noexcept
 {
-    return BODY();
-}
-
-void bar(function_ref<int() noexcept> f)
-{
-    f();
+    return BODYN(free_function);
 }
 
 struct A_good
 {
-    int g() noexcept { return BODY(); }
+    int g() noexcept { return BODYN('g'); }
 };
 
 int h_good(A) noexcept
 {
-    return BODY();
+    return BODYN('h');
 }
 
-void test_noexcept()
+suite noexcept_qualified = []
 {
-    static_assert(std::is_invocable_v<decltype(foo), decltype(f)>);
-    static_assert(not std::is_invocable_v<decltype(bar), decltype(f)>);
+    using namespace bdd;
 
-    bar(f_good);
+    feature("call will be noexcept") = []
+    {
+        given("a noexcept function") = []
+        { expect(call(f_good) == free_function); };
 
-    static_assert(std::is_invocable_v<decltype(foo), C>);
-    static_assert(not std::is_invocable_v<decltype(bar), C>);
+        given("a noexcept closure") = []
+        { expect(call([]() noexcept { return BODYN(1); }) == 1_i); };
 
-    bar([]() noexcept { return BODY(); });
+        given("a noexcept structual callable") = []
+        { expect(call(nontype<f_good>) == free_function); };
+    };
 
-    static_assert(std::is_invocable_v<decltype(foo), nontype_t<f>>);
-    static_assert(not std::is_invocable_v<decltype(bar), nontype_t<f>>);
+    feature("call from noexcept member function") = []
+    {
+        given("an object without operator()") = []
+        {
+            A_good x;
 
-    bar(nontype<f_good>);
+            when("binding by name") = [&] {
+                expect(call({nontype<&A_good::g>, x}) == ch<'g'>);
+            };
 
-    using T = function_ref<int()>;
-    using U = function_ref<int() noexcept>;
-    static_assert(std::is_constructible_v<T, nontype_t<&A::g>, A &>);
-    static_assert(not std::is_constructible_v<U, nontype_t<&A::g>, A &>);
+            when("binding by pointer") = [&] {
+                expect(call({nontype<&A_good::g>, &x}) == ch<'g'>);
+            };
 
-    A_good x;
-    bar({nontype<&A_good::g>, x});
-    bar({nontype<&A_good::g>, &x});
-    bar({nontype<&A_good::g>, std::ref(x)});
+            when("binding by reference_wrapper") = [&] {
+                expect(call({nontype<&A_good::g>, std::ref(x)}) == ch<'g'>);
+            };
+        };
 
-    A a;
-    bar({nontype<&A::data>, a});
+        given("an object without a noexcept member function") = []
+        {
+            A a;
 
-    static_assert(std::is_constructible_v<T, nontype_t<h>, A &>);
-    static_assert(not std::is_constructible_v<U, nontype_t<h>, A &>);
+            then("you can treat member access as a nothrow call") = [&] {
+                expect(call({nontype<&A::data>, a}) == 99_i);
+            };
 
-    bar({nontype<h_good>, a});
-}
+            then("you can treat a noexcept free function as its memfn") = [&] {
+                expect(call({nontype<h_good>, a}) == ch<'h'>);
+            };
+        };
+    };
+};
+
+static_assert(std::is_invocable_v<decltype(foo), decltype(f)>);
+static_assert(not std::is_invocable_v<decltype(call), decltype(f)>,
+              "function may throw");
+
+static_assert(std::is_invocable_v<decltype(foo), C>);
+static_assert(not std::is_invocable_v<decltype(call), C>,
+              "operator() may throw");
+
+static_assert(std::is_invocable_v<decltype(foo), nontype_t<f>>);
+static_assert(not std::is_invocable_v<decltype(call), nontype_t<f>>,
+              "function may throw regardless whether its pointer is constant");
+
+using T = function_ref<int()>;
+using U = function_ref<int() noexcept>;
+
+static_assert(std::is_constructible_v<T, nontype_t<&A::g>, A &>);
+static_assert(not std::is_constructible_v<U, nontype_t<&A::g>, A &>,
+              "member function may throw");
+
+static_assert(std::is_constructible_v<T, nontype_t<h>, A &>);
+static_assert(not std::is_constructible_v<U, nontype_t<h>, A &>,
+              "explicit member function may throw");
