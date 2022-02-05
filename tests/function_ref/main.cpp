a85bce25a8ddab782283f609297b3f249e076e9e
Zhihao Yuan <zhihao.yuan@broadcom.com>
2022-02-05 00:13:44 -0800

    test: convert mixed signature tests to ut

--- tests/function_ref/main.cpp
+++ tests/function_ref/main.cpp
@@ -1,10 +1,8 @@
 ﻿#include "common_callables.h"
 
-void test_const_noexcept();
 void test_call_pattern();
 
 int main()
 {
-    test_const_noexcept();
     test_call_pattern();
 }
--- tests/function_ref/test_const_noexcept.cpp
+++ tests/function_ref/test_const_noexcept.cpp
@@ -2,62 +2,99 @@
 
 struct D
 {
-    int operator()() noexcept { return BODY(); }
-    int operator()() const noexcept { return BODY(); }
+    int operator()() noexcept { return BODYN(11); }
+    int operator()() const noexcept { return BODYN(22); }
 };
 
-void dad(function_ref<int() const noexcept> f)
-{
-    f();
-}
+constexpr auto unqual_call = [](function_ref<int()> f) { return f(); };
+constexpr auto call = [](function_ref<int() const noexcept> f) { return f(); };
 
 struct A_nice
 {
-    int g() noexcept { return BODY(); }
-    int h() const noexcept { return BODY(); }
+    int g() noexcept { return BODYN('g'); }
+    int h() const noexcept { return BODYN('h'); }
 };
 
-void test_const_noexcept()
+suite const_noexcept_qualified = []
 {
-    D d;
-    D const cd;
-    foo(d);
-    dad(d);
-    foo(cd);
-    dad(cd);
+    using namespace bdd;
 
-    static_assert(
-        not std::is_invocable_v<decltype(dad), decltype([]() { return 0; })>);
-    static_assert(
-        not std::is_invocable_v<
-            decltype(dad), decltype([i = 0]() mutable noexcept { return i; })>);
+    given("a const object and a non-const object") = []
+    {
+        D d;
+        D const cd;
 
-    dad([]() noexcept { return BODY(); });
+        when("unqualified signature call non-const object") = [&]
+        { expect(unqual_call(d) == 11_i); };
 
-    A a;
-    dad({nontype<&A::data>, a});
-    dad({nontype<&A::data>, &a});
-    std::reference_wrapper r = a;
-    dad({nontype<&A::data>, r});
+        when("qualified signature call non-const object") = [&]
+        { expect(call(d) == 22_i); };
 
-    using U = function_ref<int() const noexcept>;
+        when("unqualified signature call const object") = [&]
+        { expect(unqual_call(cd) == 22_i); };
 
-    static_assert(not std::is_constructible_v<U, nontype_t<&A::data>, A>);
+        when("qualified signature call const object") = [&]
+        { expect(call(cd) == 22_i); };
+    };
 
-    static_assert(not std::is_constructible_v<U, nontype_t<&A::g>, A &>);
-    static_assert(not std::is_constructible_v<U, nontype_t<&A::k>, A &>);
-    static_assert(
-        not std::is_constructible_v<U, nontype_t<&A_nice::g>, A_nice &>);
+    given("a noexcept closure") = []
+    { expect(call([]() noexcept { return BODYN(42); }) == 42_i); };
 
-    A_nice w;
-    dad({nontype<&A_nice::h>, w});
-    dad({nontype<&A_nice::h>, &w});
-    dad({nontype<&A_nice::h>, std::as_const(w)});
+    given("a non-const object with no qualified member function") = []
+    {
+        A a;
 
-    static_assert(
-        not std::is_constructible_v<U, nontype_t<&A_nice::h>, A_nice>);
+        then("pointer to data member is deemed a pure accessor") = [&]
+        {
+            expect(call({nontype<&A::data>, a}) == 99_i);
+            expect(call({nontype<&A::data>, &a}) == 99_i);
 
-    static_assert(not std::is_constructible_v<U, nontype_t<h>, A &>);
-    int h_good(A) noexcept;
-    static_assert(std::is_constructible_v<U, nontype_t<h_good>, A &>);
-}
+            std::reference_wrapper r = a;
+            expect(call({nontype<&A::data>, r}) == 99_i);
+        };
+    };
+
+    given("a non-const object with qualified member functions") = []
+    {
+        A_nice w;
+
+        then("can call const member function") = [&]
+        {
+            expect(call({nontype<&A_nice::h>, w}) == ch<'h'>);
+            expect(call({nontype<&A_nice::h>, &w}) == ch<'h'>);
+        };
+
+        then("can call const member function on const lvalue") = [&] {
+            expect(call({nontype<&A_nice::h>, std::as_const(w)}) == ch<'h'>);
+        };
+    };
+};
+
+static_assert(
+    not std::is_invocable_v<decltype(call), decltype([] { return 0; })>,
+    "noexcept signature does not accept potentially-throwing lambda");
+
+static_assert(
+    not std::is_invocable_v<decltype(call),
+                            decltype([i = 0]() mutable noexcept { return i; })>,
+    "const noexcept signature does not accept noexcept but mutable lambda");
+
+using U = function_ref<int() const noexcept>;
+
+static_assert(not std::is_constructible_v<U, nontype_t<&A::data>, A>,
+              "cannot bind rvalue");
+
+static_assert(not std::is_constructible_v<U, nontype_t<&A::g>, A &>,
+              "not qualified");
+static_assert(not std::is_constructible_v<U, nontype_t<&A::k>, A &>,
+              "not noexcept-qualified");
+static_assert(not std::is_constructible_v<U, nontype_t<&A_nice::g>, A_nice &>,
+              "not const-qualified");
+
+static_assert(not std::is_constructible_v<U, nontype_t<&A_nice::h>, A_nice>,
+              "cannot bind rvalue");
+
+static_assert(not std::is_constructible_v<U, nontype_t<h>, A &>,
+              "not noexcept");
+int h_good(A) noexcept;
+static_assert(std::is_constructible_v<U, nontype_t<h_good>, A &>);
