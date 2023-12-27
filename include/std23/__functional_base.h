4431b9a4600b3861b37635e33927e92f1202198a
Hana Dusíková <hanicka@hanicka.net>
2023-12-27 07:23:51 +0100

    fix: template template argument mismatch in clang

--- include/std23/__functional_base.h
+++ include/std23/__functional_base.h
@@ -47,17 +47,17 @@ template<class T, class Self>
 inline constexpr bool _is_not_self =
     not std::is_same_v<std::remove_cvref_t<T>, Self>;
 
-template<class T, template<class> class>
+template<class T, template<class...> class>
 inline constexpr bool _looks_nullable_to_impl = std::is_member_pointer_v<T>;
 
-template<class F, template<class> class Self>
+template<class F, template<class...> class Self>
 inline constexpr bool _looks_nullable_to_impl<F *, Self> =
     std::is_function_v<F>;
 
 template<class... S, template<class...> class Self>
 inline constexpr bool _looks_nullable_to_impl<Self<S...>, Self> = true;
 
-template<class S, template<class> class Self>
+template<class S, template<class...> class Self>
 inline constexpr bool _looks_nullable_to =
     _looks_nullable_to_impl<std::remove_cvref_t<S>, Self>;
 
